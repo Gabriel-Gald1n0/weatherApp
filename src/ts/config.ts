1a124f9aea73b2ecb1a614acf5498f656297fcc4
gabriel <galdinobrossx1@gmail.com>
2024-08-25 15:52:19 -0300

    add config.ts

---
+++ src/ts/config.ts
@@ -0,0 +1,5 @@
+export const apiKey = 'COLE_AQUI_SUA_CHAVE_API';
+export const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+export const imgApiKey = 'COLE_AQUI_SUA_CHAVE_API';
+export const imgApiUrl = 'https://api.pexels.com/v1/search';
